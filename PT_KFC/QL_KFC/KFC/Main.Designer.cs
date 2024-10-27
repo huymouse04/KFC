e796f94e08ef3b59b56ad9448238f5e91b07e4b0
thung10 <135448009+thung10@users.noreply.github.com>
2024-10-27 16:18:37 +0700

    btn

--- PT_KFC/QL_KFC/KFC/Main.Designer.cs
+++ PT_KFC/QL_KFC/KFC/Main.Designer.cs
@@ -361,9 +361,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(135, 75);
+            this.button1.Location = new System.Drawing.Point(129, 48);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(319, 84);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
