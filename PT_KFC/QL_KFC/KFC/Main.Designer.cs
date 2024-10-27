c579ab7597305f077991e535ae06ddd8814e366b
thung10 <135448009+thung10@users.noreply.github.com>
2024-10-27 16:41:28 +0700

    test1

--- PT_KFC/QL_KFC/KFC/Main.Designer.cs
+++ PT_KFC/QL_KFC/KFC/Main.Designer.cs
@@ -47,7 +47,6 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.panel_Body = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
             this.flowLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
@@ -349,7 +348,6 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel_Body.BackgroundImage = global::KFC.Properties.Resources.banner5;
-            this.panel_Body.Controls.Add(this.button1);
             this.panel_Body.Controls.Add(this.panel1);
             this.panel_Body.Controls.Add(this.flowLayoutPanel1);
             this.panel_Body.Location = new System.Drawing.Point(0, 0);
@@ -358,15 +356,6 @@
             this.panel_Body.Size = new System.Drawing.Size(1523, 1144);
             this.panel_Body.TabIndex = 17;
             this.panel_Body.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(475, 282);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(350, 87);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // Main
             // 
@@ -405,7 +394,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel_Body;
         private CustomButton.VBButton btnThoat;
-        private System.Windows.Forms.Button button1;
     }
 }
 
